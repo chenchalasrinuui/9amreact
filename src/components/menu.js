4478ce86c400d0eb6f669b7cdf88253cfa028f0c
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2023-04-27 09:41:46 +0530

    first commit

---
+++ src/components/menu.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const Menu = () => {
+  return (
+    <ul className='menu'>
+      <li>
+        <Link href="/home">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About Us</Link>
+      </li>
+      <li>
+        <Link href="/contact">Contact</Link>
+      </li>
+    </ul>
+  )
+}
