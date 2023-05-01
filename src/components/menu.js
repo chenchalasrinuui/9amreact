b8894de51b48d630470bb9a4f2a9ecd6c4cd6e0d
Ch Srinu <chenchala.srinuui@gmail.com>
2023-05-01 09:50:48 +0530

    active menu changes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 
 export const Menu = () => {
+  const [menuItem,setMenuItem]=useState('')
+  useEffect(()=>{
+    debugger;
+      let pathName=window.location.pathname
+      setMenuItem(pathName.slice(1) || 'home')
+  },[])
+  const fnMenuClick=(eve)=>{
+       setMenuItem(eve.target.id)
+  }
   return (
     <ul className='menu'>
-      <li>
-        <Link href="/home">Home</Link>
+      <li >
+        <Link id='home' className={menuItem=='home' && 'active-menu'} onClick={fnMenuClick} href="/home">Home</Link>
       </li>
       <li>
-        <Link href="/about">About Us</Link>
+        <Link id='about' className={menuItem=='about' && 'active-menu'} onClick={fnMenuClick} href="/about">About Us</Link>
       </li>
       <li>
-        <Link href="/contact">Contact</Link>
+        <Link id='contact' className={menuItem=='contact' && 'active-menu'} onClick={fnMenuClick} href="/contact">Contact</Link>
       </li>
     </ul>
   )
